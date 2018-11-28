63b98a2c7c799eb0adb364dca5cf1fc5c9168431
Henrik Bulskov <bulskov@ruc.dk>
2018-11-28 23:16:19 +0100

    Add project files.

---
+++ Exercise4_2/DataAccessLayer/IDataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercise4_2.DomainModel;
+
+namespace Exercise4_2.DataAccessLayer
+{
+    public interface IDataService
+    {
+        List<Post> GetPosts(int page, int pageSize);
+        Post GetPost(int id);
+        List<Post> GetAnswers(int id);
+        int NumberOfQuestions();
+    }
+}
