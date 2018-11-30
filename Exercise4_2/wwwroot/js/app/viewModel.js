cf0b9b62b16a9662645ae6d76202cca0aea103ff
theKbot <trommewestermann@hotmail.com>
2018-11-30 21:54:30 +0100

    Changed a loot..

---
+++ Exercise4_2/wwwroot/js/app/viewModel.js
@@ -0,0 +1,49 @@
+﻿define(['jquery', 'knockout'], function ($, ko) {
+
+    var link = ko.observable("something");
+    var title = ko.observable("What is love");
+    var fullPost = ko.computed(function () {
+        return link() + " " + title();
+    });
+
+    var page = ko.observable("0");
+    var postArray = ko.observableArray([]);
+    var maxPage;
+    $.getJSON("api/posts", "page="+page()+"&pageSize=10", function (data) {
+        postArray(data);
+    }
+    );
+
+    
+
+    var nextPage = function () {
+        page = ko.computed(function () {
+            alert(page());
+            return parseInt(page() + 1);
+        });
+        $.getJSON("api/posts", "page=" + page() + "&pageSize=10", function (data) {
+            postArray(data);
+        }
+        );
+    }
+    var prevPage = function () {
+        page = ko.computed(function () {
+            return parseInt(page()) - 1;
+        });
+        $.getJSON("api/posts", "page=" + page() + "&pageSize=10", function (data) {
+            postArray(data);
+        }
+        );
+    }
+
+    return {
+        postArray,
+        link,
+        fullPost,
+        title,
+        nextPage,
+        prevPage,
+        page,
+        maxPage
+    };
+});
---
+++ Exercise4_2/wwwroot/js/main.js
@@ -0,0 +1,13 @@
+﻿
+
+require.config({
+    baseUrl: "js",
+    paths: {
+        jquery: "lib/jQuery/jquery.min",
+        knockout: "lib/knockout/dist/knockout.debug"
+    }
+});
+
+require(['knockout', 'app/viewModel'], function (ko, vm) {
+    ko.applyBindings(vm);
+});
