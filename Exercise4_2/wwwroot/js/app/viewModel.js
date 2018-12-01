ce2101598b285eb245e930df7d4bb71101f307a2
theKbot <trommewestermann@hotmail.com>
2018-12-01 18:57:24 +0100

    Fuck you Github

--- Exercise4_2/wwwroot/js/app/viewModel.js
+++ Exercise4_2/wwwroot/js/app/viewModel.js
@@ -1,42 +1,36 @@
 ﻿define(['jquery', 'knockout'], function ($, ko) {
 
+    //Some variables
     var link = ko.observable("something");
     var title = ko.observable("What is love");
     var fullPost = ko.computed(function () {
         return link() + " " + title();
     });
-
     var currentUI = ko.observable("postUI");
     var page = ko.observable(0);
-    var page = ko.observable("0");
-
     var postArray = ko.observableArray([]);
     var maxPage;
+
+    //Get posts from page
     $.getJSON("api/posts", "page="+page()+"&pageSize=10", function (data) {
         postArray(data);
     }
     );
 
-
+    //Goto next page
     var nextPage = function () {
         page = ko.computed(function () {
-
-            alert(page())
             return page() + 1;
-
-            alert(page());
-            return parseInt(page() + 1);
-
         });
     }
+
+    //Goto prev page
     var prevPage = function () {
         page = ko.computed(function () {
-
             return page() - 1;
-
-            return parseInt(page()) - 1;
-
         });
+    }
+
     //Change the UI
     var changeUI = function () {
         if (currentUI() === "postUI") {
@@ -47,7 +41,6 @@
         }
     }
 
-
     return {
         postArray,
         link,
@@ -56,6 +49,8 @@
         nextPage,
         prevPage,
         page,
-        maxPage
+        maxPage,
+        currentUI,
+        changeUI
     };
 });
